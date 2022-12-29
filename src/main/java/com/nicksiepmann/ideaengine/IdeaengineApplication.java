190633f525b4ee8e6a64c35feeea638bc830c9c1
Nick.Siepmann <Nick.Siepmann@57HNRQ3>
2022-12-29 13:36:43 +0000

    first commit

---
+++ src/main/java/com/nicksiepmann/ideaengine/IdeaengineApplication.java
@@ -0,0 +1,13 @@
+package com.nicksiepmann.ideaengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaengineApplication.class, args);
+	}
+
+}
